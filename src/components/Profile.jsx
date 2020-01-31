f392d4f8766d916f6f93bf3da0be96fbfdd1df14
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-01-31 22:17:41 +0600

    initial project

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return(
+        <div className='content'>
+            <div>
+                <img src='https://i.ytimg.com/vi/Co_gby7hi3U/maxresdefault.jpg' alt=''/>
+            </div>
+            <div>
+                avatar + desc
+            </div>
+            <div>
+                Post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                    <div>Post 3</div>
+                    <div>Post 3</div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Profile;
