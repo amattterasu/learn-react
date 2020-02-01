189b8fb50ea15bf83b0814d9a3c8e96e466db7bd
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-02-01 22:05:23 +0600

    added routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import s from './Navbar.module.css';
 
 const Navbar = () => {
-    return(
+    return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a>Profile</a>
+                <a href={`/profile`}>Profile</a>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a>Messages</a>
+                <a href={`/dialogs`}>Messages</a>
             </div>
             <div className={s.item}>
                 <a>News</a>
