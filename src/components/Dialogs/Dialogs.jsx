8d80281f15e542e6e3c56dcfd58889ec6b71fc8d
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-02-02 17:33:35 +0600

    added dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-  return (
-    <div>
-      Dialogs
-    </div>
-  )
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>Andrey</div>
+                <div className={`${s.dialog} ${s.active}`}>Martin</div>
+                <div className={s.dialog}>Juchara</div>
+                <div className={s.dialog}>Another Person</div>
+                <div className={s.dialog}>Some Person</div>
+                <div className={s.dialog}>Kylo Ren</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello World!</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Zabusti</div>
+            </div>
+        </div>
+    )
 };
 
 export default Dialogs;
