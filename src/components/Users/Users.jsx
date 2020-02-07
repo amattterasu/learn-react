69ab8352296121f3c2e128bb0f7415639c142092
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-02-07 16:53:39 +0600

    connect USERS API and made get request

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.jpg'
 
 let Users = (props) => {
+    if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            }
+        );
+    }
 
     return (
         <div>
@@ -9,22 +17,22 @@ let Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} alt={`Photo`} className={styles.usersPhoto}/>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} alt={`Photo must be here`} className={styles.usersPhoto}/>
                         </div>
                         <div>
-                            { u.followed
-                                ? <button onClick={() => { props.unfollow(u.id) } }>Unfollow</button>
-                                : <button onClick={() =>  {props.follow(u.id) } }>Follow</button>}
+                            {u.followed
+                                ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
+                                : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
                         </div>
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.city}</div>
-                            <div>{u.location.country}</div>
+                            <div>{"u.location.city"}</div>
+                            <div>{"u.location.country"}</div>
                         </span>
                     </span>
                 </div>)
@@ -32,5 +40,4 @@ let Users = (props) => {
         </div>
     )
 };
-
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -2,33 +2,37 @@ const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
+// let initialState = {
+//     users: [
+//         {
+//             id: 1,
+//             followed: false,
+//             fullName: 'Andrey',
+//             status: 'I am a boss',
+//             location: {city: 'Tomsk', country: 'Russia'},
+//             photoUrl: 'https://cdn24.img.ria.ru/images/155231/05/1552310590_0:405:2048:1557_600x0_80_0_0_880832a40040dfcb928722ae168cc719.jpg'
+// },
+//         {
+//             id: 2,
+//             followed: true,
+//             fullName: 'Martin',
+//             status: 'I am a looser',
+//             location: {city: 'Moscow', country: 'Russia'},
+//             photoUrl: 'https://g4.delphi.lv/images/pix/676x385/DnnPaIuKh_k/kakis-mele-laizities-50902459.jpg'
+//         },
+//         {
+//             id: 3,
+//             followed: false,
+//             fullName: 'Dmitry',
+//             status: 'I am boss too too',
+//             location: {city: 'Tomsk', country: 'Russia'},
+//             photoUrl: 'https://icdn.lenta.ru/images/2019/10/06/13/20191006135047104/pic_bf6cef2550e28d7298e7b7d441b3cdd6.jpg'
+//         }
+//     ]
+// };
+
 let initialState = {
-    users: [
-        {
-            id: 1,
-            followed: false,
-            fullName: 'Andrey',
-            status: 'I am a boss',
-            location: {city: 'Tomsk', country: 'Russia'},
-            photoUrl: 'https://cdn24.img.ria.ru/images/155231/05/1552310590_0:405:2048:1557_600x0_80_0_0_880832a40040dfcb928722ae168cc719.jpg'
-},
-        {
-            id: 2,
-            followed: true,
-            fullName: 'Martin',
-            status: 'I am a looser',
-            location: {city: 'Moscow', country: 'Russia'},
-            photoUrl: 'https://g4.delphi.lv/images/pix/676x385/DnnPaIuKh_k/kakis-mele-laizities-50902459.jpg'
-        },
-        {
-            id: 3,
-            followed: false,
-            fullName: 'Dmitry',
-            status: 'I am boss too too',
-            location: {city: 'Tomsk', country: 'Russia'},
-            photoUrl: 'https://icdn.lenta.ru/images/2019/10/06/13/20191006135047104/pic_bf6cef2550e28d7298e7b7d441b3cdd6.jpg'
-        }
-    ]
+    users: []
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -65,6 +69,6 @@ const usersReducer = (state = initialState, action) => {
 // just return object for dispatch
 export const followAC = (userId) => ({type: FOLLOW, userId});
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
-export const setUsersAC = (user) => ({type: SET_USERS, user});
+export const setUsersAC = (users) => ({type: SET_USERS, users});
 
 export default usersReducer;
