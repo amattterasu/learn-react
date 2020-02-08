58bdd1da24c16492c32ee2b62c7bf9534f38410d
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-02-08 18:48:47 +0600

    added auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
-    return(
+const Header = (props) => {
+    return (
         <header className={s.header}>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Logo_TV_2015.svg/1200px-Logo_TV_2015.svg.png' />
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Logo_TV_2015.svg/1200px-Logo_TV_2015.svg.png'/>
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
         </header>
     )
 };
