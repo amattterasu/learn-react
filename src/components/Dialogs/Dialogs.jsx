5ba64d78502012a281795d23635231ce43a87cc5
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-02-10 20:05:55 +0600

    redux-form validation

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,6 +4,8 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import {Field, reduxForm} from "redux-form";
 import {Redirect} from "react-router-dom";
+import {Textarea} from "../common/FormsControls/FormsControls";
+import {maxLengthCreator, required} from "../utils/validators/validators";
 
 const Dialogs = (props) => {
 
@@ -31,11 +33,16 @@ const Dialogs = (props) => {
     )
 };
 
+const maxLength50 = maxLengthCreator(50);
+
 const AddMessageForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field component={'textarea'} name={`newMessageBody`} placeholder={`Enter your message`}/>
+                <Field component={Textarea}
+                       name={`newMessageBody`}
+                       placeholder={`Enter your message`}
+                       validate={[required, maxLength50]}/>
             </div>
             <div>
                 <button>Send</button>
