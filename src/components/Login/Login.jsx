a0b7b00155e040cdb5c62be59f05428e74470fcc
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-02-11 18:00:19 +0600

    added stopSubmit for login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import {required} from "../utils/validators/validators";
 import {connect} from "react-redux";
 import {login} from "../../redux/auth-reducer";
 import {Redirect} from "react-router-dom";
+import styles from "../common/FormsControls/FormsControls.module.css"
 
 const LoginForm = (props) => {
     return (
@@ -18,9 +19,13 @@ const LoginForm = (props) => {
                        component={Input} validate={[required]} type={`password`}/>
             </div>
             <div>
-                <Field  name={'rememberMe'} type={`checkbox`}
-                        component={Input} validate={[required]}/> remember me
+                <Field name={'rememberMe'} type={`checkbox`}
+                       component={Input} validate={[required]}/> remember me
             </div>
+            {props.error && <div className={styles.formSummaryError}>
+                {props.error}
+            </div>
+            }
             <div>
                 <button>Login</button>
             </div>
@@ -33,10 +38,12 @@ const LoginReduxForm = reduxForm({
 })(LoginForm);
 
 const Login = (props) => {
-    const onSubmit = (formData) => { props.login(formData.email, formData.password, formData.rememberMe) };
+    const onSubmit = (formData) => {
+        props.login(formData.email, formData.password, formData.rememberMe)
+    };
 
     if (props.isAuth) {
-        return <Redirect to={`/profile`} />;
+        return <Redirect to={`/profile`}/>;
     }
 
     return <div>
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,4 +1,5 @@
 import {authAPI} from "../api/api";
+import {stopSubmit} from 'redux-form';
 
 const SET_USER_DATA = 'SET_USER_DATA';
 
@@ -23,9 +24,12 @@ const authReducer = (state = initialState, action) => {
 };
 
 // just return object for dispatch
-export const setAuthUserData = (userId, email, login, isAuth) => ({type: SET_USER_DATA, payload: {userId, email, login, isAuth}});
+export const setAuthUserData = (userId, email, login, isAuth) => ({
+    type: SET_USER_DATA,
+    payload: {userId, email, login, isAuth}
+});
 
-export const getAuthUserData = () => (dispatch) =>  {
+export const getAuthUserData = () => (dispatch) => {
     authAPI.me()
         .then(response => {
             if (response.data.resultCode === 0) {
@@ -35,16 +39,20 @@ export const getAuthUserData = () => (dispatch) =>  {
         });
 };
 
-export const login = (email, password, rememberMe) => (dispatch) =>  {
+export const login = (email, password, rememberMe) => (dispatch) => {
+
     authAPI.login(email, password, rememberMe)
         .then(response => {
             if (response.data.resultCode === 0) {
                 dispatch(getAuthUserData())
+            } else {
+                let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
+                dispatch(stopSubmit('login', {_error: message}));
             }
-        });
+        })
 };
 
-export const logout = () => (dispatch) =>  {
+export const logout = () => (dispatch) => {
     authAPI.logout()
         .then(response => {
             if (response.data.resultCode === 0) {
