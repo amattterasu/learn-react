f428e21fe43b33ec219e1c89463b601d65fe38f4
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-02-13 19:40:38 +0600

    added tdd for delete posts

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,4 +1,4 @@
-import profileReducer, {addPostActionCreator} from "./profile-reducer";
+import profileReducer, {addPostActionCreator, deletePost} from "./profile-reducer";
 import React from "react";
 
 let state = {
@@ -32,5 +32,14 @@ it('message of new post should be correct', () => {
     expect(newState.posts[4].message).toBe('NewText');
 });
 
+it('after deleting length of messages should be decrement', () => {
+    //1. test data
+    let action = deletePost(1);
 
+    //2. action
+    let newState = profileReducer(state, action);
+
+    //3. expectation
+    expect(newState.posts.length).toBe(3);
+});
 
